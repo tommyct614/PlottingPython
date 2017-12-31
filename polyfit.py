7134b60e5575e6f36dc5b32b0e2d66b72e555aba
tommyct614 <34486618+tommyct614@users.noreply.github.com>
2017-12-31 12:03:41 +0530

    Update polyfit.py

--- polyfit.py
+++ polyfit.py
@@ -1,29 +1,24 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+#Importing tools required
 
 rawData = np.loadtxt("data.dat")
 x=rawData.transpose()[0]
 y=rawData.transpose()[1]
+#Reads input from data.dat file and transposes columns into rows
 
 
 order = 9
+#Order of the polynomial to get best fit. 
+#Figure this out by trial and error
 
-coeff=np.polyfit(x,y,order)
-f=np.poly1d(coeff)
+coeff=np.polyfit(x,y,order) #Returns a least squares fit and stores it in coeff
+f=np.poly1d(coeff) #Creates a 1D polynomial class
 
-
-x_new=np.linspace(x[0],x[-1],101)
-y_new=f(x_new)
-
-
-fh=open("output.dat","wb")
-np.savetxt(fh,np.c_[order],fmt="%10d")
-np.savetxt(fh,coeff,fmt="%10.8e")
-fh.close()
-
-plt.plot(x,y, "r*",label="data")
-plt.plot(x_new,y_new, "b-",label="fit")
-plt.savefig('figure.pdf', format='pdf')
+plt.plot(x,y, "r*",label="data") #Plot x,y with red asterisks and label as data.
+plt.xlabel('numbers')
+plt.ylabel('their squares')
+plt.savefig('figure.pdf', format='pdf') #Save the figure as a pdf
 plt.show()
 
 
